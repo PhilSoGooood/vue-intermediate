8a41357d0b45803b73062087fde370688819fbe4
PhilSoGooood <peelhw@gmail.com>
2022-08-15 23:06:19 +0900

    섹션 2. Todo App - 프로젝트 구현

---
+++ vue-todo/src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+
+/* import font awesome 컴포넌트 */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/*
+   import specific icons
+   원하는 아이콘을 개별적으로 import
+   ##### faPlus 추가 #####
+*/
+import { faUserSecret, faPlus, faTrash, faCheck  } from '@fortawesome/free-solid-svg-icons'
+/*
+  위에서 import한 아이콘들을 Core library에 등록
+  ##### faPlus 추가 ######
+*/
+library.add(faUserSecret, faPlus, faTrash, faCheck )
+
+/* font awesome 컴포넌트를 전역으로 등록 */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
